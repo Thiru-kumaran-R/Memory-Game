af470c09a77aec5c1f329c99d8539063291de923
thirukumaran05 <learningthiru@gmail.com>
2023-08-25 18:38:28 +0530

    Modified Memory Game

--- index.js
+++ index.js
@@ -1,43 +1,27 @@
+const popUp = document.querySelector(".pop-up")
 const noOfMoves = document.querySelector("#moves");
 const emojiList = document.querySelectorAll("#emoji");
 const cards = document.querySelectorAll(".card");
-const time = document.querySelector("#time");
+const popUpMove = document.getElementById("move");
 const emojis = ["😍","😍","😇","😇","😵‍💫","😵‍💫", "🙂","🙂", "🥹","🥹","🥴","🥴","🫠","🫠","🥵","🥵"];
 const shuffleEmoji = emojis.sort( ()=> (Math.random() > 0.5) ? 2 : -1);
-var startGame = false ;
-var counter = 0;
+
 var lengthOfList = emojiList.length;
 var count = 0;
 
-
+//Create board
 for(var i = 0 ; i < lengthOfList ; i++){
     var currentCard = emojiList[i] ;
     var currentEmoji = shuffleEmoji[i];
     currentCard.innerText = ` ${currentEmoji}`;
 }
 
- function startCountDown(number){
-        
-    var timer = setInterval(() =>{
-        time.innerText = `${counter}s`;
-        console.log(counter);
-        counter++;
-    }, 1000)
 
-    if(number == 14){
-        clearInterval(timer)
-    }
-}
-        
-if(startGame){
-    startCountDown()
-}
-
+//Flip card on user click
 cards.forEach(element =>{
     element.addEventListener("click", ()=>{
         element.classList.add("flip-card");
         count++;
-        startGame = true ;
 
         const moves = document.getElementById("moves");
         moves.innerText = count ;
@@ -45,6 +29,7 @@ cards.forEach(element =>{
         var userChoice = document.querySelectorAll(".flip-card") ;
         var userChoiceList = document.querySelectorAll(".swap-card");
 
+        //To check whether selected cards are equal or not
         setTimeout(function(){
 
             if(userChoice.length > 1){
@@ -61,10 +46,8 @@ cards.forEach(element =>{
 
 
                     if(userChoiceList.length == 14){
-                        alert("Congratulations, You Won!") ;
-                        startGame = false ;
-                        window.location.reload();
-
+                        popUp.classList.add("open");
+                        popUpMove.innerText = count;
                     }
                 }else{
                     userChoice[1].classList.remove("flip-card");
@@ -72,7 +55,7 @@ cards.forEach(element =>{
                 }
             }
         }, 400)
-    
+
     })
 })
 
